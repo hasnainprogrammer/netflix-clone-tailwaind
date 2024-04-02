765ffeb50dadd61bdd8971eac2771dbc9fc19305
hasnainprogrammer <hasnainprogrammer@gmail.com>
2024-04-02 16:06:43 +0500

    netflix clone v0.0

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/*.html"],
+  theme: {
+    extend: {
+      colors: {
+        netflixRed: 'rgb(229, 9, 20)'
+      }
+     
+    },
+  },
+  plugins: [],
+}
+
